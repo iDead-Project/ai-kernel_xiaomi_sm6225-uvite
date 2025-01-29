845469fd851e7bb54f1c9a2668925ee4a9ca3499
kammarih <quic_kammarih@quicinc.com>
2025-01-29 22:00:51 +0530

    kernel: misc: heartbeat: Enable MCU communication in bengal_2w_v2
    Enable MCU communication in bengal_2w_v2 as it is supported.
    Added functionality to send heartbeat events from kernel space to user
    space.
    
    Change-Id: I09f577e70cc1f04969866a309f6d0e4579983a4b
    Signed-off-by: kammarih <quic_kammarih@quicinc.com>

--- drivers/misc/heartbeat/heartbeat.c
+++ drivers/misc/heartbeat/heartbeat.c
@@ -1,5 +1,5 @@
 //SPDX-License-Identifier: GPL-2.0-only
-//Copyright (c) 2024 Qualcomm Innovation Center, Inc. All rights reserved.
+//Copyright (c) 2024-2025 Qualcomm Innovation Center, Inc. All rights reserved.
 
 #include<linux/init.h>
 #include<linux/module.h>
@@ -13,120 +13,255 @@
 #include<linux/mutex.h>
 #include<linux/heartbeat.h>
 
-#define QTI_EVENT_TIMEOUT        3
-#define HB_BUFFER_SIZE           1024
-#define HB_PROBE_MAX_RETRY_CNT   3
-#define HB_DATA_LENGTH           4
+#define QTI_EVENT_TIMEOUT                3
+#define HB_BUFFER_SIZE                   1024
+#define HB_PROBE_MAX_RETRY_CNT           3
+#define HB_DATA_LENGTH                   4
+#define HB_SYSSTATE_FAIL                 1
+#define TRIGGER_HB_BUF_SIZE              9
+#define HB_REG_WITH_QTI_CAN_SUCCESS      0
+#define HB_REG_WITH_QTI_CAN_FAIL         1
+#define HB_QTI_CAN_DATA_INVALID          1
 
-struct kobject *h_kobj;
-uint32_t sysstate_value = 0;
-struct delayed_work hwork;
-struct mutex h_lock;
-void *qti_can_priv_data = NULL;
-char *hb_buf = NULL;
+typedef struct {
+	struct kobject *hb_kobj;
+	struct kobj_attribute hb_attr;
+	struct delayed_work hb_work;
+	struct mutex hb_lock;
+	void *qti_can_priv_data;
+	char *hb_buf;
+	bool mcu_present;
+	bool trigger_hb_event;
+	char *trigger_hb_buf;
+	u8 hb_probe_retry_count;
+} heartbeat;
+
+uint32_t sysstate_value;
+static heartbeat *hb_priv_data;
 
 int send_heartbeat_event(void *, uint32_t, int);
 
 void trigger_heartbeat_event(const char *driver_name, uint32_t state)
 {
+	int ret;
         pr_debug("%s: driver_name(%s), state(0x%x)", __func__, driver_name, state);
-        if(!qti_can_priv_data) {
-                pr_err("%s: qti_can_priv_data is NULL", __func__);
-                return;
-        }
-        mutex_lock(&h_lock);
-        sysstate_value = state;
-        send_heartbeat_event(qti_can_priv_data, sysstate_value, HB_DATA_LENGTH);
-        mutex_unlock(&h_lock);
-        sysstate_value = 0;
+	mutex_lock(&hb_priv_data->hb_lock);
+	sysstate_value = state;
+	hb_priv_data->trigger_hb_event = true;
+	snprintf(hb_priv_data->trigger_hb_buf, TRIGGER_HB_BUF_SIZE,
+			"%8x", state);
+	if (hb_priv_data->mcu_present) {
+		if (!hb_priv_data->qti_can_priv_data) {
+			pr_err("%s: qti_can_priv_data is NULL", __func__);
+			mutex_unlock(&hb_priv_data->hb_lock);
+			return;
+		}
+		ret = send_heartbeat_event(hb_priv_data->qti_can_priv_data,
+				sysstate_value, HB_DATA_LENGTH);
+		if (ret) {
+			pr_err("%s: heartbeat event spi transaction failed ret(%d)",
+					__func__, ret);
+		}
+	}
+
+	sysfs_notify(hb_priv_data->hb_kobj, NULL, "sysstate_value");
+	sysstate_value = 0;
+	mutex_unlock(&hb_priv_data->hb_lock);
 }
 
 void send_qti_events(struct work_struct *work)
 {
-	mutex_lock(&h_lock);
-	send_heartbeat_event(qti_can_priv_data, sysstate_value, HB_DATA_LENGTH);
-	mutex_unlock(&h_lock);
+	int ret;
+
+	mutex_lock(&hb_priv_data->hb_lock);
 	sysstate_value = 0;
-	schedule_delayed_work(&hwork, QTI_EVENT_TIMEOUT*HZ);
+	ret = send_heartbeat_event(hb_priv_data->qti_can_priv_data,
+			sysstate_value, HB_DATA_LENGTH);
+	if (ret) {
+		pr_err("%s: heartbeat event spi transaction failed ret(%d)",
+				__func__, ret);
+	}
+
+	mutex_unlock(&hb_priv_data->hb_lock);
+	schedule_delayed_work(&hb_priv_data->hb_work, QTI_EVENT_TIMEOUT*HZ);
 }
 
 static ssize_t android_status_show(struct kobject *kobj,
 		struct kobj_attribute *attr, char *buf)
 {
-	return snprintf(buf, HB_BUFFER_SIZE, "%s", hb_buf);
+	if (hb_priv_data->trigger_hb_event) {
+		hb_priv_data->trigger_hb_event = false;
+		return snprintf(buf, HB_BUFFER_SIZE, "%s",
+				hb_priv_data->trigger_hb_buf);
+	} else {
+		return snprintf(buf, HB_BUFFER_SIZE, "%s",
+				hb_priv_data->hb_buf);
+	}
 }
 
 static ssize_t android_status_store(struct kobject *kobj,
 		struct kobj_attribute *attr, const char *buf, size_t count)
 {
+	int ret;
 	if (count >= HB_BUFFER_SIZE) {
 		return -EINVAL;
 	}
-	strlcpy(hb_buf, buf, count);
+	strlcpy(hb_priv_data->hb_buf, buf, count);
+	mutex_lock(&hb_priv_data->hb_lock);
 	sscanf(buf, "%8x", &sysstate_value);
-	mutex_lock(&h_lock);
-	send_heartbeat_event(qti_can_priv_data, sysstate_value, HB_DATA_LENGTH);
-	mutex_unlock(&h_lock);
-	sysstate_value = 0;
-	return count;
-}
-
-static struct kobj_attribute h_attr = __ATTR(sysstate_value, 0664, android_status_show, android_status_store);
-
-int register_heartbeat(void *priv_data) {
-	qti_can_priv_data = priv_data;
-	return 0;
-}
-EXPORT_SYMBOL(register_heartbeat);
-
-static int qti_heartbeat_probe(struct platform_device *pdev) {
-	int ret = 0;
-	static int retry;
-
-	if (!qti_can_priv_data) {
-		if (retry < HB_PROBE_MAX_RETRY_CNT) {
-			pr_err("%s: trying to probe retry count %d\n", __func__, retry);
-			retry++;
-			return -EPROBE_DEFER;
-		} else {
-			ret = -1;
-			return ret;
+	if (hb_priv_data->mcu_present) {
+		ret = send_heartbeat_event(hb_priv_data->qti_can_priv_data,
+				sysstate_value, HB_DATA_LENGTH);
+		if (ret) {
+			pr_err("%s: heartbeat event spi transaction failed ret(%d)",
+					__func__, ret);
 		}
 	}
 
-	h_kobj = kobject_create_and_add("qti_heartbeat",NULL);
-	if (!h_kobj) {
-		ret = -ENOMEM;
-		return ret;
+	mutex_unlock(&hb_priv_data->hb_lock);
+	return count;
+}
+
+int register_heartbeat(void *qti_can_priv_data)
+{
+	if (qti_can_priv_data) {
+		hb_priv_data->qti_can_priv_data = qti_can_priv_data;
+		return HB_REG_WITH_QTI_CAN_SUCCESS;
+	}
+	return -HB_REG_WITH_QTI_CAN_FAIL;
+}
+EXPORT_SYMBOL(register_heartbeat);
+
+static int mcu_is_present(struct device *hb_dev)
+{
+	struct device_node *hb_dev_node;
+	int mcu_present = 0;
+
+	if (!hb_dev) {
+		pr_err("%s: Heartbeat device is not exist", __func__);
+		return -ENODEV;
 	}
 
-	hb_buf = (char *)kzalloc(HB_BUFFER_SIZE, GFP_KERNEL);
-	if (!hb_buf) {
-		kobject_put(h_kobj);
+	hb_dev_node = hb_dev->of_node;
+	if (!hb_dev_node) {
+		pr_err("%s: Heratbeat node is not exist", __func__);
+		return -ENODEV;
+	}
+
+	mcu_present = of_property_read_bool(hb_dev_node, "mcu-present");
+	return mcu_present;
+}
+
+static struct kobj_attribute hb_attr = __ATTR(sysstate_value, 0664, android_status_show, android_status_store);
+
+static int create_hb_sysfs_entry(void)
+{
+	int ret = 0;
+
+	hb_priv_data->hb_kobj = kobject_create_and_add("qti_heartbeat", NULL);
+	if (!hb_priv_data->hb_kobj) {
+		pr_err("%s: kobject qti_heartbeat creation failed", __func__);
 		return -ENOMEM;
 	}
 
-	ret = sysfs_create_file(h_kobj, &h_attr.attr);
+	sysstate_value = 0;
+	ret = sysfs_create_file(hb_priv_data->hb_kobj, &hb_attr.attr);
 	if (ret) {
-		kfree(hb_buf);
-		kobject_put(h_kobj);
-		pr_err("%s Failed to create /sys/qti_heartbeat/sysstate_value file\n",__func__);
-		ret = -1;
-		return ret;
+		kobject_put(hb_priv_data->hb_kobj);
+		pr_err("%s Failed to create /sys/qti_heartbeat/sysstate_value file",
+				__func__);
+		return -HB_SYSSTATE_FAIL;
+	}
+	return ret;
+}
+
+static int allocate_hb_buffers(void)
+{
+	int ret = 0;
+
+	hb_priv_data->hb_buf = (char *)kzalloc(HB_BUFFER_SIZE, GFP_KERNEL);
+	if (!hb_priv_data->hb_buf) {
+		pr_err("%s: Memory allocation failed for hb_buf",
+				__func__);
+		return -ENOMEM;
 	}
 
-	mutex_init(&h_lock);
+	hb_priv_data->trigger_hb_buf = (char *)kzalloc(TRIGGER_HB_BUF_SIZE, GFP_KERNEL);
+	if (!hb_priv_data->trigger_hb_buf) {
+		kfree(hb_priv_data->hb_buf);
+		pr_err("%s: Memory allocation failed for trigger_hb_buf",
+				__func__);
+		return -ENOMEM;
+	}
+	return ret;
+}
 
-	INIT_DELAYED_WORK(&hwork, send_qti_events);
-	schedule_delayed_work(&hwork, QTI_EVENT_TIMEOUT*HZ);
-	pr_info("%s completed\n",__func__);
+static int qti_heartbeat_probe(struct platform_device *pdev)
+{
+	int ret = 0;
+
+	if (!hb_priv_data) {
+		hb_priv_data = (heartbeat *)kzalloc(sizeof(heartbeat), GFP_KERNEL);
+	}
+
+	if (!hb_priv_data) {
+		pr_err("%s: Memory allocation failed to heartbeat",
+				__func__);
+		ret = -ENOMEM;
+		goto exit;
+	}
+
+	hb_priv_data->mcu_present = mcu_is_present(&pdev->dev);
+	if (hb_priv_data->mcu_present) {
+		if (!hb_priv_data->qti_can_priv_data) {
+			if (hb_priv_data->hb_probe_retry_count < HB_PROBE_MAX_RETRY_CNT) {
+				pr_err("%s: trying to hb probe retry count %d",
+						__func__, hb_priv_data->hb_probe_retry_count);
+				hb_priv_data->hb_probe_retry_count++;
+				return -EPROBE_DEFER;
+			} else {
+				kfree(hb_priv_data);
+				pr_err("%s: heartbeat probe failed on invalid \
+						qti_can_data", __func__);
+				ret = -HB_QTI_CAN_DATA_INVALID;
+				goto exit;
+			}
+		}
+	}
+
+	ret = create_hb_sysfs_entry();
+	if (ret) {
+		kfree(hb_priv_data);
+		goto exit;
+	}
+
+	ret = allocate_hb_buffers();
+	if (ret) {
+		sysfs_remove_file(hb_priv_data->hb_kobj, &hb_attr.attr);
+		kobject_put(hb_priv_data->hb_kobj);
+		kfree(hb_priv_data);
+		goto exit;
+	}
+
+	mutex_init(&hb_priv_data->hb_lock);
+	if (hb_priv_data->mcu_present) {
+		INIT_DELAYED_WORK(&hb_priv_data->hb_work, send_qti_events);
+		schedule_delayed_work(&hb_priv_data->hb_work, QTI_EVENT_TIMEOUT*HZ);
+	}
+	pr_info("%s completed", __func__);
+exit:
 	return ret;
 }
 
 static int qti_heartbeat_remove(struct platform_device *pdev) {
-	kfree(hb_buf);
-	kobject_put(h_kobj);
-	cancel_delayed_work_sync(&hwork);
+	kfree(hb_priv_data->hb_buf);
+	kfree(hb_priv_data->trigger_hb_buf);
+	sysfs_remove_file(hb_priv_data->hb_kobj, &hb_attr.attr);
+	kobject_put(hb_priv_data->hb_kobj);
+	if (hb_priv_data->mcu_present) {
+		cancel_delayed_work_sync(&hb_priv_data->hb_work);
+	}
+	kfree(hb_priv_data);
 	return 0;
 }
 
