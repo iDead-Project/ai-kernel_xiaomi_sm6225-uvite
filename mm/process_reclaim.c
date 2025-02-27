819e06eb600a65f104967b179d16ac7b30bd42c8
iDeadXS <datarafi43@gmail.com>
2025-02-27 14:30:05 +0000

    mm: prlmk: Cleanup remains

--- mm/process_reclaim.c
+++ mm/process_reclaim.c
@@ -240,7 +240,6 @@ static void sort_and_kill_tasks(struct selected_task selected[], int si)
 		if (tsk->mm) {
 			if (!test_bit(MMF_OOM_SKIP, &tsk->mm->flags)) {
 				mark_lmk_victim(tsk);
-				wake_oom_reaper(tsk);
 			}
 		}
 		task_unlock(tsk);
@@ -249,8 +248,7 @@ static void sort_and_kill_tasks(struct selected_task selected[], int si)
 				max,
 				(si + 1),
 				tsk->comm,
-				tsk->signal->oom_score_adj,
-				cputime_to_nsecs(tsk->acct_timexpd));
+				tsk->signal->oom_score_adj);
 
 		msleep(20);
 		if (tsk)
